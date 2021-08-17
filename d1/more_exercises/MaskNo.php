346b5ae37e09aee76b17319aba2a4d0f948584b2
Tulika66 <52247742+Tulika66@users.noreply.github.com>
2021-08-17 17:03:15 +0530

    update masknumber

--- d1/more_exercises/MaskNo.php
+++ d1/more_exercises/MaskNo.php
@@ -1,14 +1,22 @@
 <?php
 
-function masker($number){
-
+function masker($number,$maskFrom){
     $length=strlen($number);
     if($length!=10){
         return "Not Valid Phone Number";
     }
 
+
+    //maskFrom=2 ;=> first two letters mask out and last two
+    if($maskFrom>=5 || $maskFrom==0){
+        return $number;
+    }
+    if($maskFrom<0){
+        return "Enter valid masking length";
+    }
+
     for($iterator=0;$iterator<strlen($number);$iterator++){
-       if($iterator<=1 || $iterator>=($length-2)){continue;}
+       if($iterator<=($maskFrom-1) || $iterator>=($length-$maskFrom)){continue;}
        $number[$iterator]="*";
 
     }
@@ -17,4 +25,4 @@ function masker($number){
 }
 
 $number="9876543210";
-echo masker($number);
+echo masker($number,5);
